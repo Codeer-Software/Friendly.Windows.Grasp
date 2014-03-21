3843165cf14b74e43028180db9c0e496e9ff20b5
ishikawa-tatsuya <ishikawa-tatsuya@codeer.co.jp>
2014-03-21 23:12:11 +0900

    Friendly2.2対応→Grasp2.2.2

--- Project/Nuget/MakeNupack.bat
+++ Project/Nuget/MakeNupack.bat
@@ -1,1 +1,4 @@
+rd /s /q "../ReleaseBinary"
+"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" "../Codeer.Friendly.Windows.Grasp/Codeer.Friendly.Windows.Grasp.sln" /rebuild Release
+"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" "../Codeer.Friendly.Windows.Grasp/Codeer.Friendly.Windows.Grasp.sln" /rebuild Release-Eng
 nuget pack friendly.windows.grasp.nuspec
